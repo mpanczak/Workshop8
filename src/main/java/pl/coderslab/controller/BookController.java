f50af5376be6d524707d4f69b9ddf6070792b518
mpanczak <panczakmateusz@gmail.com>
2023-06-06 16:40:23 +0200

    fork from Workshop7

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,52 @@
+package pl.coderslab.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import pl.coderslab.dao.BookService;
+import pl.coderslab.simple.Book;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public
+    List<Book> getList() {
+        return bookService.getBooks();
+    }
+
+    @PostMapping("")
+    public void addBook(@RequestBody Book book) {
+        bookService.add(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return this.bookService.get(id).orElseThrow(() -> {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        });
+    }
+
+    @PutMapping("")
+    @ResponseBody
+    public void updateBook(@RequestBody Book book) {
+        bookService.update(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeBook(@PathVariable Long id) {
+        bookService.delete(id);
+    }
+
+}
