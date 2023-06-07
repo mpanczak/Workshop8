d7d772e4daed75e931eb72b4f21479f8fcc1d267
mpanczak <panczakmateusz@gmail.com>
2023-06-07 19:24:39 +0200

    BookRepository

---
+++ src/main/java/pl/coderslab/service/JpaBookService.java
@@ -0,0 +1,46 @@
+package pl.coderslab.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import pl.coderslab.dao.BookService;
+import pl.coderslab.entity.Book;
+import pl.coderslab.repository.BookRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Primary
+public class JpaBookService implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public JpaBookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public void add(Book book) {
+
+    }
+
+    @Override
+    public Optional<Book> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void update(Book book) {
+
+    }
+}
